a4bc7090d8fa9f95634d5adb4415f55aace742e1
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-04-01 18:29:34 +0200

    list resources

--- src/clj/knakk/svamp/routes.clj
+++ src/clj/knakk/svamp/routes.clj
@@ -1,11 +1,12 @@
 (ns knakk.svamp.routes
   "Application HTTP routes."
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer [GET defroutes context]]
             [compojure.route :as route]
             [compojure.handler :as handler]
             [clojure.java.io :as io]
             [clojure.edn :as edn])
   (:import [java.io.PushbackReader]))
+
 
 ;; Utility functions ==========================================================
 
@@ -22,16 +23,41 @@
    :headers {"Content-Type" "application/edn"}
    :body (pr-str data)})
 
+
 ;; Resources ==================================================================
 
-(def settings (atom (load-edn "settings.edn")))
 (defn page-settings [] (io/resource "html/settings.html"))
 (defn page-resources [] (io/resource "html/resources.html"))
+(defn page-new-resource [type] (io/resource "html/resource-new.html"))
+
+(def settings (atom (load-edn "settings.edn")))
+(def rdf-types
+  ;; TODO error handling:
+  ;;   edn/read-string will fail if input is not well-formed EDN
+  (let [files (->> "rdf-types"
+                    io/resource
+                    io/file
+                    file-seq
+                    (filter #(.isFile %)))]
+    (vec (map (comp #(select-keys % [:label :desc :rdf-type])
+                    edn/read-string slurp) files))))
+
+
 
 ;; Routing ====================================================================
 
 (defroutes approutes
+  ;; API
   (GET "/api/settings" [] (api-response @settings))
+  (GET "/api/rdf-types" [] (api-response
+                             (let [files (->> "rdf-types" io/resource io/file
+                                              file-seq (filter #(.isFile %)))]
+                               (vec (map (comp #(select-keys % [:label :desc :rdf-type])
+                                               edn/read-string slurp) files)))))
+
+  ;; Web interface
   (GET "/resources" [] (page-resources))
+  (GET "/resource-new" [rdf-type] (page-new-resource rdf-type))
   (GET "/settings"[] (page-settings))
+
   (route/not-found "Nothing here. Try elsewhere."))
---
+++ src/cljs/knakk/svamp/resources.cljs
@@ -0,0 +1,41 @@
+(ns knakk.svamp.resources
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan alts! <!]]
+            [knakk.svamp.utils :refer [edn-xhr]]))
+
+(enable-console-print!)
+
+(defn metadata
+  [data owner]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (edn-xhr
+        {:method :get
+         :url "api/rdf-types"
+         :on-complete #(om/update! data %)}))
+    om/IRender
+    (render [_]
+      (dom/div nil
+        (dom/div nil
+          (dom/h3 nil "Create a new resource")
+          (apply dom/ul #js {:className "monospace row resources"}
+            (map (fn [e]
+                  (dom/a #js {:href (str "resource-new?rdf-type=" (:rdf-type e))}
+                     (dom/li #js {:className "column one-third"}
+                             (dom/strong nil (:label e))
+                             (dom/span #js {:className "dark-gray"} (:desc e)))))
+                 data)))
+        (dom/div nil
+          (dom/h3 nil "Modify an existing resource")
+          (dom/div #js {:className "monospace"}
+            (dom/input #js {:type "text" :placeholder "Search for resources"})
+            (dom/span nil " of type ")
+            (apply dom/select nil
+              (map (fn [e]
+                     (dom/option #js {:value (:rdf-type e)} (:label e)))
+                   (into [{:label "Any" :rdf-type "any"}] data)))))))))
+
+(om/root metadata {} {:target (. js/document (getElementById "page-app-1"))})
