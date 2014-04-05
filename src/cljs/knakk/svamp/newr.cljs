5beab45066e34a68c5ae7fb5914515e672c3ed46
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-04-05 13:18:21 +0200

    fix multi-predicate input
    fix multi-predicate input

--- src/cljs/knakk/svamp/newr.cljs
+++ src/cljs/knakk/svamp/newr.cljs
@@ -115,11 +115,39 @@
             (dom/span #js {:className "delete mrgh" :onClick #(om/update! value :value nil)} "x")))
         (om/build uri-search-bar value {:init-state {:chosen-chan chosen-chan}})))))
 
+(defn multi-uri-input
+  "Component: Input element where value must be a URI and where predicates can
+  be chosen from a dropdown list. Includes search-bar."
+  [value owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:chosen "" :chosen-chan (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [chosen-chan (om/get-state owner :chosen-chan)]
+        (go
+          (loop []
+            (let [c (<! chosen-chan)]
+              (om/set-state! owner :chosen c))
+            (recur)))))
+    om/IRenderState
+    (render-state [_ {:keys [chosen chosen-chan delete-chan predicates]}] ;; TODO delete-chan
+      (dom/div #js {:className "relative row"}
+        (dom/div #js {:className "column half"}
+          (dom/input #js {:value (:value value) :disabled true :type "text" :title chosen})
+          (when (seq (:value value))
+            (dom/span #js {:className "delete mrgh" :onClick #(om/update! value :value nil)} "x"))
+          (apply dom/select nil
+            (map (fn [p] (dom/option #js {:value (:predicate p)} (:label p))) predicates)))
+        (om/build uri-search-bar value {:init-state {:chosen-chan chosen-chan}})))))
+
 ;; Dispatch on input type
 (defmulti input-type (fn [value _] (:type value)))
 (defmethod input-type :integer [value owner] (literal-input value owner)) ;; TODO make number-input, and also textarea-input
 (defmethod input-type :string [value owner] (literal-input value owner))
 (defmethod input-type :uri [value owner] (uri-input value owner))
+(defmethod input-type :multi-uri [value owner] (multi-uri-input value owner))
 
 (defn single-view
   [element owner]
@@ -136,7 +164,8 @@
         (apply dom/div nil
           (om/build-all input-type
                         (:values element)
-                        {:init-state {:create-new (:create-new element)}}))
+                        {:init-state {:create-new (:create-new element)
+                                      :predicates (:predicates element)}}))
         (when (:repeatable element)
           (dom/button #js {:className "addElement"
                            :disabled (not-every? #(seq (:value %)) (:values element))
@@ -144,33 +173,6 @@
                                       (om/transact! element :values
                                                     #(conj % (:value-template @element))))}
                       "+"))))))
-
-(defn multi-view
-  [element owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-      {:chosen ""})
-    om/IRenderState
-    (render-state [_ {:keys [chosen]}]
-      (dom/div #js {:className "resource monospace row"}
-        (dom/div nil
-          (dom/div #js {:className "elementTitle"}
-            (dom/label nil (:label element))
-            (when (:required element)
-              (dom/span #js {:className "red"} "*")))
-          (dom/div #js {:className "column half"}
-            (dom/input #js {:value (:value element) :disabled true :type "text" :title chosen})
-            (when (seq (:value element))
-              (dom/span #js {:className "delete mrgh" :onClick #(om/update! element :value nil)} "x"))
-            (apply dom/select nil
-              (map (fn [p] (dom/option #js {:value (:predicate p)} (:label p))) (:predicates element))))
-          (om/build uri-search-bar element))))))
-
-;; Dispatch on variable predicate or not
-(defmulti element-view (fn [element _] (:multi-predicates element)))
-(defmethod element-view false [element owner] (single-view element owner))
-(defmethod element-view true [element owner] (multi-view element owner))
 
 (defn input-group
   [group owner]
@@ -180,7 +182,7 @@
       (dom/div nil
         (dom/h2 nil (:title group))
         (apply dom/div nil
-          (om/build-all element-view (:elements group)))))))
+          (om/build-all single-view (:elements group)))))))
 
 (defn assoc-value [data]
   ;; TODO refactor this, unreadable.
