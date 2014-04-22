8264a6762537e539f88ac4d9479e1de2e5ed6a11
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-04-22 16:41:20 +0200

    todo: wait til july 2014 for getty TGN as linked data

--- resources/resource-types/place.clj
+++ resources/resource-types/place.clj
@@ -1,6 +1,9 @@
 {
  :rdf-type "http://data.deichman.no/format/Place"
  :index-type "place"
+ :index-mappings {}
+ :uri-fn (fn [{:keys [getty-id]}]
+           (str "<http://vocab.getty.edu/tgn/" (->> getty-id first :value) ">"))
  :label "Place"
  :desc "a point in space."
  :groups []
